b5abb6f12136a5e420269662a93be2e86c2dc0bd
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2020-12-28 00:35:59 +0500

    Palindrome Number with an even better solution

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,18 +1,44 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
-        temp = x
-        
-        if temp < 0:
+        #Numbers less than 0 or ending with a 0 can never be palindrome number
+        if x<0 or (x%10 == 0 and x!= 0):
             return False
+       
         
         res = 0
         
-        while temp:
-            res = res*10 + temp%10
-            temp = int(temp/10)
+        '''
+
+        Even Number
+        1212 
+        res = 12
+        x = 12
+        loop terminates
+
+        
+        Odd Number
+        12312
+        x = 123
+        res = 12
+        loop terminates
+
+        Loop executes until x>res .... that is when the mid of the number is reached
+
+
+
+        '''
+
+
+        while x>res:
+            res = res*10 + x%10
+            x = int(x/10)
             
-        return res == x
+        return x == res or x == int(res/10)
+        
+        
+            
+       
             
         
        
