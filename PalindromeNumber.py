3ca5400d82c22881c95a7c156adaab727d981b3e
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2020-12-28 00:08:28 +0500

    A better clean solution to Palindrome Number problem

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,32 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        
         temp = x
         
-        if temp<0:
+        if temp < 0:
             return False
-        elif temp>= 0 and temp<10:
-            return True
         
         res = 0
-        while temp != 0:
-            
-            pop = self.mod(temp)
-            temp = self.divide(temp)
-            
-            
-            res = res*10 + pop
-            
-        if res == x:
-            return True
-        else:
-            return False
-            
-            
-            
-            
-            
-    def divide(self,number):
-        return int(number/10)
-    def mod(self,number):
-        return number%10
         
+        while temp:
+            res = res*10 + temp%10
+            temp = int(temp/10)
+            
+        return res == x
+            
+        
+       
