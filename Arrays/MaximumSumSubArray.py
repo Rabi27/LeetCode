4229a7ee398c32d0ea30496f9b2fd899661e5a04
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2021-01-12 19:31:52 +0500

    Solution of 53. Maximum Subarray ..... Kadane's Algorithm ...O(n) complexity

--- Arrays/MaximumSumSubArray.py
+++ Arrays/MaximumSumSubArray.py
@@ -22,17 +22,27 @@ Output: 0
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        curr_sum = 0
+        class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        curr_sum = nums[0]
         max_sum = nums[0]
-        for i in range(len(nums)):
+        
+        for i in range(1,len(nums)):
+            curr_sum = max(nums[i],curr_sum+nums[i])
             
-            curr_sum = 0
-            for j in range(i,len(nums)):
-                
-                curr_sum += nums[j]
-                max_sum = max(curr_sum,max_sum)
+            if curr_sum> max_sum:
+                max_sum = curr_sum
                 
         return max_sum
+        
+        
+        
+    
+        
+        
+      
+        
         
         
     
