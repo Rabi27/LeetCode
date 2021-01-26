c6fbd1dc802993d82176cda971491dd9759758a9
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2021-01-26 20:15:10 +0500

    1695. Maximum Erasure Value

--- Arrays/MaximumErasureValue.py
+++ Arrays/MaximumErasureValue.py
@@ -1,32 +1,53 @@
+'''
+You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,4,5,6]
+Output: 17
+Explanation: The optimal subarray here is [2,4,5,6].
+Example 2:
+
+Input: nums = [5,2,1,2,5,2,1,2,5]
+Output: 8
+Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+'''
 class Solution:
-    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+    def maximumUniqueSubarray(self, arr: List[int]) -> int:
         
+        n = len(arr)
         i = 0
-        j = 1
+        j = 0
         
-        subarr = [nums[0]]
-        maxsize = 1
-        arr = []
+        s = set()
+        curr_sum = 0
+        max_sum = 0
         
-        while j < len(nums):
+        while j<n:
             
-            if nums[j] not in subarr:
-                subarr.append(nums[j])
-            elif nums[j] in subarr:
-                if len(subarr) > maxsize:
-                    arr.append(subarr)
-                    maxsize = len(subarr)
-                i = subarr.index(nums[j])
-                subarr = subarr[i+1:]
-                subarr.append(nums[j])
-            j += 1
+            if arr[j] in s:
+                s.remove(arr[i])
+                curr_sum-=arr[i]
+                i+=1
+            else:
+                s.add(arr[j])
+                curr_sum+=arr[j]
+                max_sum = max(max_sum, curr_sum)
+                j+=1
         
-        arr.append(subarr) 
-        m = 1
-        for l in arr:
-            m = max(sum(l),m)
-        return m
-            
+        return max_sum          
                 
         
         
