f5d8cf4abb5bbbc94d20606cb96b3ab0a54371d3
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2021-01-27 23:04:11 +0500

    162. Find Peak Element....OneLiner

--- Arrays/PeakElement.py
+++ Arrays/PeakElement.py
@@ -56,14 +56,5 @@ Explanation: Your function can return either index number 1 where the peak eleme
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
-        if len(nums) == 1: return 0
-        
-        if nums[0] > nums[1]: return 0
-        if nums[-1] > nums[-2]: return len(nums) - 1
-        
-        for i in range(1,len(nums)-1):
-            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
-                return i
-            
-        return -1
+        return nums.index(max(nums))
         
