45f26f9af4073e985e9b82eca1221999f4569bdf
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2021-02-08 21:27:53 +0500

    140. Word Break II

---
+++ Recursion/WordBreakII.py
@@ -0,0 +1,62 @@
+'''
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+  "cats and dog",
+  "cat sand dog"
+]
+Example 2:
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+  "pine apple pen apple",
+  "pineapple pen apple",
+  "pine applepen apple"
+]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input:
+s = "catsandog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output:
+[]
+'''
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+        memo = {}
+        def allCombinations(target,words):
+            if target in memo: return memo[target]
+            if target == "": return [""]
+            res = []
+            for word in words:
+
+                if target.find(word) == 0:
+                    suffix = target[len(word):]
+                    combinations = allCombinations(suffix,words)
+                    for c in combinations:
+                        if c != "":
+                            res.append(word+" "+c)
+                        else:
+                            res.append(word)
+
+            memo[target] = res
+            return res
+        
+        return allCombinations(s,wordDict)
+        
